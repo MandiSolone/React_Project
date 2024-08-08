bcc7e2ce0383dc35acac43c414bbf7bd310514b2
[Mandi Solone] <[mandisolone@gmail.com]>
2024-08-08 15:59:31 -0400

    added useState for form submission

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"; 
+
 export default function HomePage() {
-  return (
+  
+    const [movieTitleEntered, setMovieTitleEntered] = useState(""); 
+    const [movieList, setMovieList] = useState([]); 
+   
+    return ( 
     <div>
-      <h1>TEST CHANGE Movie Watchlist</h1>
-      <form action="/submit-form">
+      <h1>Movie Watchlist</h1>
+      <form action="/submit-form" onSubmit={(event) => {
+        event.preventDefault();
+        setMovieList([...movieList, movieTitleEntered]);
+        setMovieTitleEntered('');
+      }} >
         <label htmlFor="movie-want-to-watch">Movie you would like to watch:</label>
         <input
           type="text"
           id="movie-name"
           name="movie-name"
+          value = {movieTitleEntered}
+          onChange={(event) => setMovieTitleEntered(event.target.value)}
         />
         <button className="submit" type="button">
           Add Movie to Watch List
@@ -17,6 +29,8 @@ export default function HomePage() {
         <li>It Ends With Us</li>
         <li>Despicable Me 4</li>
         <li>Twister</li>
+        {movieList.map((ul, movieList) => (
+          <li key ={movieList}>{ul}</li>))}
       </ul>
     </div>
   );
